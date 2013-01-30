47e68b10327d8e54cf00bbbdb94ca085db0a7670
OmeGa <omega@mailoo.org>
2013-01-29 20:58:53 -0600

    Add index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Language definitions used in index.php
+$lang_index = array(
+
+'Topics'		=>	'Temas',
+'Link to'		=>	'Enlace a:', // As in "Link to: http://fluxbb.org/"
+'Empty board'	=>	'El foro está vacío.',
+'Newest user'	=>	'Último usuario registrado: %s',
+'Users online'	=>	'Usuarios registrados conectados: %s',
+'Guests online'	=>	'Invitados conectados: %s',
+'No of users'	=>	'Número total de usuarios registrados: %s',
+'No of topics'	=>	'Número total de temas: %s',
+'No of posts'	=>	'Número total de mensajes: %s',
+'Online'		=>	'Conectado:', // As in "Online: User A, User B etc."
+'Board info'	=>	'Información del foro',
+'Board stats'	=>	'Estadísticas del foro',
+'User info'		=>	'Información del usuario'
+
+);
