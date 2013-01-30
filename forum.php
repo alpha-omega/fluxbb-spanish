13eaee20731f53dbe78bb26e9624326e3443d1fa
OmeGa <omega@mailoo.org>
2013-01-29 20:58:25 -0600

    Add forum.php

---
+++ forum.php
@@ -0,0 +1,17 @@
+<?php
+
+// Language definitions used in viewforum.php
+$lang_forum = array(
+
+'Post topic'	=>	'Publicar nuevo tema',
+'Views'			=>	'Vistas',
+'Moved'			=>	'Movido:',
+'Sticky'		=>	'Importante:',
+'Closed'		=>	'Cerrado:',
+'Empty forum'	=>	'Foro vacío.',
+'Mod controls'	=>	'Controles de moderación',
+'Is subscribed'	=>	'Actualmente estás suscrito a este foro',
+'Unsubscribe'	=>	'Eliminar suscripción',
+'Subscribe'		=>	'Suscribirse'
+
+);
