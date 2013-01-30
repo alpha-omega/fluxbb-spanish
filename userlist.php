25e047cc4bbf449536bd69e6f76ec9ab22971d40
OmeGa <omega@mailoo.org>
2013-01-29 21:02:36 -0600

    Add userlist.php

---
+++ userlist.php
@@ -0,0 +1,13 @@
+<?php
+
+// Language definitions used in userlist.php
+$lang_ul = array(
+
+'User find legend'	=>	'Encuentra y ordena usuarios',
+'User search info'	=>	'Introduce un nombre de usuario para buscar y/o un grupo de usuarios para filtrar. El campo de nombre de usuario no puede quedar vacío. Utiliza un carácter comodín (*) para coincidencias parciales.',
+'User sort info'	=>	'Ordena a los usuarios por nombre, fecha de registro o número de mensajes, y en orden ascendente/descendente.',
+'User group'		=>	'Grupo de usuarios',
+'No of posts'		=>	'Número de mensajes',
+'All users'			=>	'Todos'
+
+);
