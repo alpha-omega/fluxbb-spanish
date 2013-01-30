ac05f90d8cb70ea6ca1f64c0215362a54f22b22e
OmeGa <omega@mailoo.org>
2013-01-29 20:59:21 -0600

    Add login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+
+// Language definitions used in login.php
+$lang_login = array(
+
+// Miscellaneous
+'Wrong user/pass'			=>	'Nombre de usuario y/o contraseña incorrectos.',
+'Forgotten pass'			=>	'¿Olvidaste tu contraseña?',
+'Login redirect'			=>	'Entraste exitosamente. Redirigiendo…',
+'Logout redirect'			=>	'Saliste exitosamente. Redirigiendo…',
+'No email match'			=>	'No existe ningún usuario registrado con esa dirección de correo electrónico',
+'Request pass'				=>	'Solicitar contraseña',
+'Request pass legend'		=>	'Ingresa la dirección de correo electrónico con al cual te registrarte',
+'Request pass info'			=>	'Una nueva contraseña junto con un enlace para activar la nueva contraseña se enviará a esa dirección.',
+'Not registered'			=>	'¿Aún no te has registrado?',
+'Login legend'				=>	'Ingresa tu nombre de usuario y contraseña a continuación',
+'Remember me'				=>	'Entrar automáticamente en cada visita.',
+'Login info'				=>	'Si no te has registrado, o has olvidado tu contraseña, haz clic en el enlace apropiado a continuación.',
+'New password errors'		=>	'Error al solicitar la contraseña',
+'New passworderrors info'	=>	'Se necesita corregir el siguiente error antes de que una nueva contraseña pueda ser enviada:',
+
+// Forget password mail stuff
+'Forget mail'				=>	'Se ha enviado un correo electrónico a la dirección especificada con instrucciones sobre cómo cambiar tu contraseña. Si no llega puedes contactar al administrador del foro en',
+'Email flood'				=>	'Esta cuenta ya ha solicitado el restablecimiento de la contraseña en la última hora. Por favor espera %s minutos antes de solicitar una nueva otra vez.'
+
+);
