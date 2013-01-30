9c80c1378cc225e9fd9471005aae563c21667d36
OmeGa <omega@mailoo.org>
2013-01-29 23:20:14 -0600

    Add admin_forums.php

---
+++ admin_forums.php
@@ -0,0 +1,53 @@
+<?php
+
+// Language definitions used in admin_forums.php
+$lang_admin_forums = array(
+
+'Forum added redirect'		=>	'Foro añadido. Redirigiendo…',
+'Forum deleted redirect'	=>	'Foro eliminado. Redirigiendo…',
+'Forums updated redirect'	=>	'Foros actualizados. Redirigiendo…',
+'Forum updated redirect'	=>	'Foro actualizado. Redirigiendo…',
+'Perms reverted redirect'	=>	'Permisos restablecidos a los ajustes predeterminados. Redirigiendo…',
+'Must enter name message'	=>	'Debes ingresar un nombre para el foro.',
+'Must be integer message'	=>	'La posición debe ser un valor entero positivo.',
+'New forum'					=>	'Nuevo foro',
+
+// Entry page
+'Add forum head'			=>	'Añadir foro',
+'Create new subhead'		=>	'Crear un nuevo foro',
+'Add forum label'			=>	'Añadir un foro a una categoría',
+'Add forum help'			=>	'Selecciona la categoría a la cual deseas añadir un nuevo foro.',
+'Add forum'					=>	'Añadir foro',
+'No categories exist'		=>	'No existen categorías',
+'Edit forums head'			=>	'Editar foros',
+'Category subhead'			=>	'Categoría:',
+'Forum label'				=>	'Foro',
+'Edit link'					=>	'Editar',
+'Delete link'				=>	'Eliminar',
+'Position label'			=>	'Posición',
+'Update positions'			=>	'Actualizar posiciones',
+'Confirm delete head'		=>	'Confirmar la eliminación del foro',
+'Confirm delete subhead'	=>	'¡Importante! Leer antes de eliminar',
+'Confirm delete info'		=>	'¿Estás seguro de querer eliminar el foro <strong>%s</strong>?',
+'Confirm delete warn'		=>	'¡ADVERTENCIA! ¡Eliminar un foro borrará todos los mensajes (si existen) en dicho foro!',
+
+// Detailed edit page
+'Edit forum head'			=>	'Editar foro',
+'Edit details subhead'		=>	'Editar los detalles del foro',
+'Forum name label'			=>	'Nombre del foro',
+'Forum description label'	=>	'Descripción (HTML)',
+'Category label'			=>	'Categoría',
+'Sort by label'				=>	'Ordenar temas por',
+'Last post'					=>	'Último mensaje',
+'Topic start'				=>	'Inicio de tema',
+'Subject'					=>	'Asunto',
+'Redirect label'			=>	'URL de redirección',
+'Redirect help'				=>	'Solo disponible en foros vacíos',
+'Group permissions subhead'	=>	'Editar los permisos de grupo en este foro',
+'Group permissions info'	=>	'En este formulario puedes establecer los permisos específicos del foro para los distintos grupos de usuarios. Si no has hecho ningún cambio a los permisos de grupo de este foro, lo que ves debajo son las opciones predeterminadas basadas en los ajustes de %s. Los administradores tienen siempre todos los permisos, y por tanto se excluyen. Los ajustes de permisos que difieren de los permisos por defecto para el grupo de usuarios están marcados con rojo. La casilla de verificación del permiso «Leer foro» estará deshabilitada si el grupo en cuestión no cuenta con permiso para «Leer foro». Para foros de redirección, únicamente el permiso de «Leer foro» es modificable.',
+'Read forum label'			=>	'Leer foro',
+'Post replies label'		=>	'Publicar respuestas',
+'Post topics label'			=>	'Publicar temas',
+'Revert to default'			=>	'Revertir a los ajustes predeterminados',
+
+);
