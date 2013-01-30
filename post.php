51b2549a305679d12ddcfbf8ac287899e60f898a
OmeGa <omega@mailoo.org>
2013-01-29 20:59:53 -0600

    Add post.php

---
+++ post.php
@@ -0,0 +1,38 @@
+<?php
+
+// Language definitions used in post.php and edit.php
+$lang_post = array(
+
+// Post validation stuff (many are similiar to those in edit.php)
+'No subject'		=>	'Los temas deben contener un asunto.',
+'No subject after censoring'	=>	'Los temas deben contener un asunto. Luego de aplicar un filtro de censura, tu asunto quedó vació.',
+'Too long subject'	=>	'Los asuntos no pueden tener más de 70 caracteres.',
+'No message'		=>	'Debes introducir un mensaje.',
+'No message after censoring'	=>	'Debes introducir un mensaje. Luego de aplicar un filtro de censura, tu mensaje quedó vacío.',
+'Too long message'	=>	'Los mensajes no pueden ser mayores a %s bytes.',
+'All caps subject'	=>	'Los asuntos no pueden contener letras mayúsculas únicamente.',
+'All caps message'	=>	'Los mensajes no pueden contener letras mayúsculas únicamente.',
+'Empty after strip'	=>	'Parece que tu mensaje consiste solo de BBCodes vacíos. Es posible que esto sucediera porque, por ejemplo, la cita interior fue descartada a causa del nivel máximo de profundidad en las citas.',
+
+// Posting
+'Post errors'		=>	'Errores en el mensaje',
+'Post errors info'	=>	'Los siguientes errores deben ser corregidos antes de que el mensaje pueda ser publicado:',
+'Post preview'		=>	'Vista previa del mensaje',
+'Guest name'		=>	'Nombre', // For guests (instead of Username)
+'Post redirect'		=>	'Mensaje enviado. Redirigiendo…',
+'Post a reply'		=>	'Responder',
+'Post new topic'	=>	'Nuevo tema',
+'Hide smilies'		=>	'Nunca mostrar emoticonos como íconos para este mensaje',
+'Subscribe'			=>	'Suscribirse a este tema',
+'Stay subscribed'	=>	'Permanecer suscrito a este tema',
+'Topic review'		=>	'Revisión de temas (los más recientes primero)',
+'Flood start'		=>	'Deben pasar al menos %s segundos entre mensajes. Por favor espera %s segundos, e inténtalo de nuevo.',
+'Preview'			=>	'Vista previa', // submit button to preview message
+
+// Edit post
+'Edit post legend'	=>	'Editar el mensaje y enviar los cambios',
+'Silent edit'		=>	'Edición silenciosa (no mostrar «Editado por ...» en la vista del tema)',
+'Edit post'			=>	'Editar mensaje',
+'Edit redirect'		=>	'Mensaje actualizado. Redirigiendo…'
+
+);
