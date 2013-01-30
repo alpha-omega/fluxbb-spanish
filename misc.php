2fa9a05f38fc564d45ec0af948672c2e6219848e
OmeGa <omega@mailoo.org>
2013-01-29 20:59:32 -0600

    Add misc.php

---
+++ misc.php
@@ -0,0 +1,93 @@
+<?php
+
+// Language definitions used in various scripts
+$lang_misc = array(
+
+'Mark read redirect'			=>	'Todos los temas y foros se han marcado como leídos. Redirigiendo…',
+'Mark forum read redirect'		=>	'Todos los temas en el foro especificado se han marcado como leídos. Redirigiendo…',
+
+// Send email
+'Form email disabled'			=>	'El usuario al cual intentas enviarle un correo electrónico ha deshabilitado el formulario de correo.',
+'No email subject'				=>	'Debes introducir un asunto.',
+'No email message'				=>	'Debes introducir un mensaje.',
+'Too long email message'		=>	'Los mensajes no pueden contener más de 65535 caracteres (65 KB).',
+'Email flood'					=>	'Deben pasar al menos %s entre el envío de correos. Por favor espera %s segundos e intenta de nuevo.',
+'Email sent redirect'			=>	'Correo electrónico enviado. Redirigiendo …',
+'Send email to'					=>	'Enviar un correo electrónico a',
+'Email subject'					=>	'Asunto',
+'Email message'					=>	'Mensaje',
+'Email disclosure note'			=>	'Por favor ten en cuenta que al utilizar este formulario tu dirección de correo electrónico será revelada al destinatario.',
+'Write email'					=>	'Escribe y envía tu mensaje de correo electrónico',
+
+// Report
+'No reason'						=>	'Debes especificar una razón.',
+'Reason too long'				=>	'Tu mensaje debe ser menor a 65535 bytes (~64kb).',
+'Report flood'					=>	'Deben pasar al menos %s entre reportes. Por favor espera %s segundos e intenta de nuevo.',
+'Report redirect'				=>	'Mensaje reportado. Redirigiendo…',
+'Report post'					=>	'Reportar mensaje',
+'Reason'						=>	'Razón',
+'Reason desc'					=>	'Por favor introduce una breve razón por la cual estás reportando este mensaje',
+
+// Subscriptions
+'Already subscribed topic'		=>	'Ya estás suscrito a este tema.',
+'Already subscribed forum'		=>	'Ya estás suscrito a este foro.',
+'Subscribe redirect'			=>	'Tu suscripción se ha añadido. Redirigiendo…',
+'Not subscribed topic'			=>	'No estás suscrito a este tema.',
+'Not subscribed forum'			=>	'No estás suscrito a este foro.',
+'Unsubscribe redirect'			=>	'Tu suscripción se ha eliminado. Redirigiendo…',
+
+// General forum and topic moderation
+'Moderate'						=>	'Moderar',
+'Select'						=>	'Seleccionar', // the header of a column of checkboxes
+'Move'							=>	'Mover',
+'Split'							=>	'Dividir',
+'Delete'						=>	'Eliminar',
+'Merge'							=>	'Unir',
+
+// Moderate forum
+'Open'							=>	'Abrir',
+'Close'							=>	'Cerrar',
+'Move topic'					=>	'Mover tema',
+'Move topics'					=>	'Mover temas',
+'Move legend'					=>	'Selecciona el destino para mover',
+'Move to'						=>	'Mover a',
+'Nowhere to move'				=>	'No hay foros a los cuales puedas mover temas.',
+'Leave redirect'				=>	'Dejar tema(s) de redirección',
+'Move topic redirect'			=>	'Tema movido. Redirigiendo…',
+'Move topics redirect'			=>	'Temas movidos. Redirigiendo…',
+'Confirm delete legend'			=>	'Por favor confirma la eliminación',
+'Delete topics'					=>	'Eliminar temas',
+'Delete topics comply'			=>	'¿Estás seguro de querer eliminar los temas seleccionados?',
+'Delete topics redirect'		=>	'Temas eliminados. Redirigiendo…',
+'Open topic redirect'			=>	'Tema abierto. Redirigiendo…',
+'Open topics redirect'			=>	'Temas cerrados. Redirigiendo…',
+'Close topic redirect'			=>	'Tema cerrado. Redirigiendo…',
+'Close topics redirect'			=>	'Temas cerrados. Redirigiendo…',
+'No topics selected'			=>	'Debes seleccionar al menos un tema para mover/eliminar/abrir/cerrar.',
+'Not enough topics selected'	=>	'Debes seleccionar al menos dos temas para unir.',
+'Stick topic redirect'			=>	'Tema marcado como importante. Redirigiendo…',
+'Unstick topic redirect'		=>	'Tema marcado como normal. Redirigiendo…',
+'Merge topics'					=>	'Unir temas',
+'Merge topics redirect'			=>	'Temas unidos. Redirigiendo…',
+'Confirm merge legend'			=>	'Por favor confirma la unión',
+'New subject'					=>	'Nuevo asunto',
+
+// Split multiple posts in topic
+'Confirm split legend'			=>	'Por favor confirma la división de los mensajes seleccionados, y selecciona el destino para mover.',
+'Split posts'					=>	'Dividir mensajes',
+'Split posts comply'			=>	'¿Estás seguro de querer dividir los mensajes seleccionados?',
+'Split posts redirect'			=>	'Mensajes divididos. Redirigiendo…',
+
+// Delete multiple posts in topic
+'Delete posts'					=>	'Eliminar mensajes',
+'Cannot select first'			=>	'El primer mensaje no se puede seleccionar para dividir/eliminar.',
+'Delete posts comply'			=>	'¿Estás seguro de querer eliminar los mensajes seleccionados?',
+'Delete posts redirect'			=>	'Mensajes eliminados. Redirigiendo…',
+'No posts selected'				=>	'Debes seleccionar al menos un mensaje para dividir/eliminar.',
+
+// Get host
+'Host info 1'					=>	'La dirección IP es: %s',
+'Host info 2'					=>	'El nombre del servidor es: %s',
+'Show more users'				=>	'Mostrar más usuarios con esta dirección IP',
+
+);
