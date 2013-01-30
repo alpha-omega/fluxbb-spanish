80a27680fa0dcad412938bbd43643e1e21783940
OmeGa <omega@mailoo.org>
2013-01-29 23:19:48 -0600

    Add admin_censoring.php

---
+++ admin_censoring.php
@@ -0,0 +1,21 @@
+<?php
+
+// Language definitions used in admin_censoring.php
+$lang_admin_censoring = array(
+
+'Must enter word message'	=>	'Debes ingresar una palabra para censurar.',
+'Word updated redirect'		=>	'Palabra censurada actualizada. Redirigiendo…',
+'Word added redirect'		=>	'Palabra censurada añadida. Redirigiendo…',
+'Word removed redirect'		=>	'Palabra censurada añadida. Redirigiendo…',
+'Censoring head'			=>	'Censura',
+'Add word subhead'			=>	'Añadir palabra',
+'Add word info'				=>	'Ingresa una palabra que desees censurar, y el texto que reemplazará a la palabra. Los comodines son aceptados (es decir, *uno* coincidirá con «alguno» y «ninguno»). Las palabras censuradas afectan también a los nombres de usuario. Los nuevos usuarios no se podrán registrar con nombres de usuario que contengan palabras censuradas. La búsqueda no distingue entre mayúsculas y minúsculas.',
+'Censoring enabled'			=>	'<strong>La censura está activada en %s.</strong>',
+'Censoring disabled'		=>	'<strong>La censura está desactivada en %s.</strong>',
+'Censored word label'		=>	'Palabra censurada',
+'Replacement label'			=>	'Palabra(s) de reemplazo',
+'Action label'				=>	'Acción',
+'Edit remove subhead'		=>	'Editar o eliminar palabras',
+'No words in list'			=>	'No hay palabras censuradas en la lista.',
+
+);
