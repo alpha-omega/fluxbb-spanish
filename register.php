d9d39f5c4fd2dfa1a119406ed13924cb01c2aa9a
OmeGa <omega@mailoo.org>
2013-01-29 21:01:21 -0600

    Add register.php

---
+++ register.php
@@ -0,0 +1,37 @@
+<?php
+
+// Language definitions used in register.php
+$lang_register = array(
+
+// Miscellaneous
+'No new regs'				=>	'Este foro no acepta nuevos registros.',
+'Reg cancel redirect'		=>	'Registro cancelado. Redirigiendo…',
+'Forum rules'				=>	'Reglas del foro',
+'Rules legend'				=>	'Debes estar de acuerdo con lo siguiente para poder registrarte',
+'Registration flood'		=>	'Un nuevo usuario se ha registrado con la misma dirección IP en la última hora. Para prevenir los registros masivos, debe pasar al menos una hora entre registros con la misma IP. Disculpa el inconveniente.',
+'Agree'						=>	'Acepto',
+'Cancel'					=>	'Cancelar',
+'Register'					=>	'Registro',
+
+// Form validation stuff (some of these are also used in post.php)
+'Registration errors'		=>	'Errores de registro',
+'Registration errors info'	=>	'Los siguientes errores deben ser corregidos antes de que te puedas registrar:',
+'Username censor'			=>	'El nombre de usuario que ingresaste contiene una o más palabras censuradas. Por favor elige un nombre de usuario distinto.',
+'Username dupe 1'			=>	'Alguien ya se ha registrado con ese nombre de usuario',
+'Username dupe 2'			=>	'El nombre de usuario que ingresaste es muy similar. El nombre de usuario debe diferir al menos por un carácter alfanumérico (a-z o 0-9). Por favor elige un nombre de usuario distinto.',
+'Email not match'			=>	'La dirección de correo electrónico no coincide.',
+
+// Registration email stuff
+'Reg email'					=>	'Gracias por registrarte. Tu contraseña se ha enviado a la dirección especificada. Si no llega, puedes contactar al administrador del foro en',
+'Reg complete'				=>	'Registro completo. Entrando y redirigiendo…',
+
+// Register info
+'Desc 1'					=>	'El registro te dará acceso a un número de funciones y capacidades que de otro modo no estarían disponibles. Estas funciones incluyen la habilidad de editar y eliminar mensajes, diseñar tu propia firma, la cual acompaña a tus mensajes, y mucho más. Si tienes preguntas sobre este foro, deberías consultar con un administrador.',
+'Desc 2'					=>	'A continuación se muestra un formulario que debes llenar para poder registrarte. Una vez registrado, deberías visitar tu perfil y revisar los distintos ajustes que puedes modificar. Los campos de abajo solo son una pequeña parte de todos los ajustes que puedes cambiar en tu perfil.',
+'Username legend'			=>	'Por favor elige un nombre de usuario que tenga entre 2 y 25 caracteres de longitud',
+'Pass legend'				=>	'Por favor introduce y confirma tu contraseña',
+'Pass info'					=>	'Las contraseñas deben tener al menos 4 caracteres de longitud. Las contraseñas distinguen entre mayúsculas y minúsculas.',
+'Email info'				=>	'Debes ingresar una dirección de correo electrónico válida, ya que tu contraseña generada aleatoriamente se enviará a dicha dirección.',
+'Confirm email'				=>	'Confirmar dirección de correo electrónico',
+
+);
