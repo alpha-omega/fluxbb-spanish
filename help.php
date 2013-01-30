006748bb036843655ba704ff5c4884d7e338768f
OmeGa <omega@mailoo.org>
2013-01-29 20:58:38 -0600

    Add help.php

---
+++ help.php
@@ -0,0 +1,66 @@
+<?php
+
+// Language definitions used in help.php
+$lang_help = array(
+
+'Help'					=>	'Ayuda',
+'produces'				=>	'produce',
+
+'BBCode'				=>	'BBCode',
+'BBCode info 1'			=>	'BBCode es una colección de etiquetas de formato que se utiliza para cambiar el aspecto del texto en este foro. BBCode está basado en el mismo principio que HTML, siendo a la vez muy similar. A continuación se muestra una lista de todos los códigos de BBCode disponibles, y las instrucciones para utilizarlos.',
+'BBCode info 2'			=>	'Los administradores tienen la habilidad de habilitar o deshabilitar el BBCode. Puedes darte cuenta de si está habilitado o no mirando la esquina inferior izquierda, siempre que publiques un nuevo mensaje o edites tu firma.',
+
+'Text style'			=>	'Estilo de texto',
+'Text style info'		=>	'Las siguientes etiquetas cambian la apariencia del texto:',
+'Bold text'				=>	'Texto en negrita',
+'Underlined text'		=>	'Texto subrayado',
+'Italic text'			=>	'Texto en cursiva',
+'Strike-through text'	=>	'Texto tachado',
+'Red text'				=>	'Texto rojo',
+'Blue text'				=>	'Texto azul',
+'Heading text'			=>	'Texto de Encabezado',
+'Deleted text'			=>	'Texto eliminado',
+'Inserted text'			=>	'Texto insertado',
+'Emphasised text'		=>	'Texto enfatizado',
+
+'Links and images'		=>	'Enlaces e imágenes',
+'Links info'			=>	'Puedes crear enlaces a otros documentos o direcciones de correo electrónico utilizando las siguientes etiquetas:',
+'This help page'		=>	'Esta página de ayuda',
+'My email address'		=>	'Mi dirección de correo electrónico',
+'Images info'			=>	'Si quieres que se muestre una imagen, puedes utilizar la etiqueta img. El texto que aparece después del signo = en la etiqueta de apertura es usado para el atributo alt, y debería incluirse siempre que sea posible.',
+'FluxBB bbcode test'	=>	'Prueba de bbcode FluxBB',
+
+'Test topic'			=>	'Tema de prueba',
+'Test post'				=>	'Mensaje de prueba',
+'Test forum'			=>	'Foro de prueba',
+'Test user'				=>	'Usuario de prueba',
+
+'Quotes'				=>	'Citas',
+'Quotes info'			=>	'Si deseas citar a alguien, debes incluir la etiqueta quote.',
+'Quotes info 2'			=>	'Si no quieres citar a alguien en particular, puedes utilizar la etiqueta quote sin especificar un nombre.',
+'Quote text'			=>	'Este es el texto que quiero citar.',
+'produces quote box'	=>	'produce una caja de cita como la siguiente:',
+'quote note'			=>	'Nota: Si un nombre de usuario contiene los caracteres [ o ], puedes colocarlo entre comillas.',
+
+'Code'					=>	'Código',
+'Code info'				=>	'Cuando quieras añadir código fuente asegúrate de utilizar la etiqueta code. El texto mostrado con la etiqueta code utilizará una tipografía monoespaciada, y no se verá afectado por otras etiquetas.',
+'Code text'				=>	'Esto es ejemplo de código.',
+'produces code box'		=>	'produce una caja de código como esta:',
+
+'Nested tags'			=>	'Etiquetas anidadas',
+'Nested tags info'		=>	'El BBCode se puede anidar para crear un formato más avanzado. Por ejemplo:',
+'Bold, underlined text'	=>	'Texto en negrita y subrayado',
+
+'Lists'					=>	'Listas',
+'List info'				=>	'Para crear una lista puedes utilizar la etiqueta list. Es posible crear tres tipos de listas usando la etiqueta list.',
+'List text 1'			=>	'Artículo de lista 1.',
+'List text 2'			=>	'Artículo de lista 2.',
+'List text 3'			=>	'Artículo de lista 3.',
+'produces list'			=>	'produce una lista con balas.',
+'produces decimal list'	=>	'produce una lista numerada.',
+'produces alpha list'	=>	'produces una lista ordenada alfabéticamente.',
+
+'Smilies'				=>	'Emoticonos',
+'Smilies info'			=>	'Si lo deseas (y si está habilitado), el foro puede convertir una serie de emoticonos a sus representaciones correspondientes en imágenes. Este foro reconoce los siguientes emoticonos, y los reemplaza por imágenes:'
+
+);|
