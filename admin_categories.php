7d0348f8c563a2ebd377062ffa0b2ffae58f56c3
OmeGa <omega@mailoo.org>
2013-01-29 23:19:37 -0600

    Add admin_categories.php

---
+++ admin_categories.php
@@ -0,0 +1,29 @@
+<?php
+
+// Language definitions used in admin_categories.php
+$lang_admin_categories = array(
+
+'Must enter name message'		=>	'Debes ingresar un nombre para la categoría',
+'Category added redirect'		=>	'Categoría añadida. Redirigiendo…',
+'Category deleted redirect'		=>	'Categoría eliminada. Redirigiendo…',
+'Delete category head'			=>	'Eliminar categoría (junto con todos los foros y mensajes que contiene)',
+'Confirm delete subhead'		=>	'Confirmar la eliminación de la categoría',
+'Confirm delete info'			=>	'¿Estás seguro de que quieres eliminar la categoría <strong>%s</strong>?',
+'Delete category warn'			=>	'<strong>¡ADVERTENCIA!</strong> ¡Eliminar una categoría borrará todos los foros y mensajes (si existen) en esta categoría!',
+'Must enter integer message'	=>	'La posición debe ser un valor entero positivo.',
+'Categories updated redirect'	=>	'Categoría actualizada. Redirigiendo…',
+'Add categories head'			=>	'Añadir categorías',
+'Add categories subhead'		=>	'Añadir categorías',
+'Add category label'			=>	'Añadir una nueva categoría',
+'Add new submit'				=>	'Añadir nueva',
+'Add category help'				=>	'El nombre de la nueva categoría que deseas agregar. Puedes editar el nombre de la categoría más tarde (ver abajo). Dirígete a %s para añadir foros a tu nueva categoría.',
+'Delete categories head'		=>	'Eliminar categorías',
+'Delete categories subhead'		=>	'Eliminar categorías',
+'Delete category label'			=>	'Eliminar una categoría',
+'Delete category help'			=>	'Selecciona el nombre de la categoría que deseas eliminar. Se te pedirá que confirmes tu elección antes de que sea eliminada.',
+'Edit categories head'			=>	'Editar categorías',
+'Edit categories subhead'		=>	'Editar categorías',
+'Category position label'		=>	'Posición',
+'Category name label'			=>	'Nombre',
+
+);
