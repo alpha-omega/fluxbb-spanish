8f18a22e2762a584662ca17b5cfc7aeee39ce4ad
OmeGa <omega@mailoo.org>
2013-01-29 21:02:11 -0600

    Add topic.php

---
+++ topic.php
@@ -0,0 +1,32 @@
+<?php
+
+// Language definitions used in viewtopic.php
+$lang_topic = array(
+
+'Post reply'		=>	'Responder',
+'Topic closed'		=>	'Tema cerrado',
+'From'				=>	'Desde:', // User location
+'IP address logged'	=>	'Dirección IP registrada',
+'Note'				=>	'Nota:', // Admin note
+'Posts'				=>	'Mensajes:',
+'Registered'		=>	'Registrado:',
+'Replies'			=>	'Respuestas:',
+'Website'			=>	'Sitio Web',
+'Guest'				=>	'Invitado',
+'Online'			=>	'Conectado',
+'Offline'			=>	'Desconectado',
+'Last edit'			=>	'Última edición por',
+'Report'			=>	'Reportar',
+'Delete'			=>	'Eliminar',
+'Edit'				=>	'Editar',
+'Quote'				=>	'Citar',
+'Is subscribed'		=>	'Actualmente estás suscrito a este tema',
+'Unsubscribe'		=>	'Eliminar suscripción',
+'Subscribe'			=>	'Suscribirse a este tema',
+'Quick post'		=>	'Respuesta rápida',
+'Mod controls'		=>	'Controles de moderación',
+'New icon'			=>	'Nuevo mensaje',
+'Re'				=>	'Re:',
+'Preview'			=>	'Vista previa'
+
+);
