e815b86f8b1b56ae3852718e9782aa5cc8df31b7
OmeGa <omega@mailoo.org>
2013-01-29 23:20:03 -0600

    Add admin_common.php

---
+++ admin_common.php
@@ -0,0 +1,44 @@
+<?php
+
+// Language definitions used in admin_common.php
+$lang_admin_common = array(
+
+// The menu
+'Admin menu'			=>	'Menú de administración',
+'Plugins menu'			=>	'Menú de complementos',
+'Moderator menu'		=>	'Menú de moderación',
+'Index'					=>	'Índice',
+'Categories'			=>	'Categorías',
+'Forums'				=>	'Foros',
+'Users'					=>	'Usuarios',
+'User groups'			=>	'Grupos de usuario',
+'Options'				=>	'Opciones',
+'Permissions'			=>	'Permisos',
+'Censoring'				=>	'Censura',
+'Bans'					=>	'Suspensiones',
+'Prune'					=>	'Purgar',
+'Maintenance'			=>	'Mantenimiento',
+'Reports'				=>	'Reportes',
+'Server statistics'				=>	'Estadísticas del servidor',
+
+'Admin'					=>	'Administración',
+'Go back'				=>	'Regresar',
+'Delete'				=>	'Eliminar',
+'Update'				=>	'Actualizar',
+'Add'					=>	'Añadir',
+'Edit'					=>	'Editar',
+'Remove'				=>	'Remover',
+'Yes'					=>	'Sí',
+'No'					=>	'No',
+'Save changes'			=>	'Guardar cambios',
+'Save'					=>	'Guardar',
+'here'					=>	'aquí',
+'Action'				=>	'Acción',
+'None'					=>	'Ninguno',
+'Maintenance mode'		=>	'modo de mantenimiento', // Used for link text in more than one file
+
+// Admin loader
+'No plugin message'		=>	'No existe ningún complemento llamado %s en el directorio de complementos.',
+'Plugin failed message'	=>	'La carga del complemento - <strong>%s</strong> - falló.',
+
+);
