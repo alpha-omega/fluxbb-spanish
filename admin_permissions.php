d951f694aa58ce58b8e161845cd7c2b4e943bbf7
OmeGa <omega@mailoo.org>
2013-01-30 18:17:21 -0600

    Add admin_permissions.php

---
+++ admin_permissions.php
@@ -0,0 +1,36 @@
+<?php
+
+// Language definitions used in admin_permissions.php
+$lang_admin_permissions = array(
+
+'Perms updated redirect'	=>	'Permisos actualizados. Redirigiendo…',
+'Permissions head'			=>	'Permisos',
+'Posting subhead'			=>	'Mensajes',
+'BBCode label'				=>	'BBCode',
+'BBCode help'				=>	'Permitir el uso de BBCode en los mensajes (recomendado).',
+'Image tag label'			=>	'Etiqueta img',
+'Image tag help'			=>	'Permitir el uso de la etiqueta BBCode [img][/img] en los mensajes.',
+'All caps message label'	=>	'Mensajes usando solo mayúsculas',
+'All caps message help'		=>	'Permitir que un mensaje contenga solo letras mayúsculas.',
+'All caps subject label'	=>	'Asuntos usando solo mayúsculas',
+'All caps subject help'		=>	'Permitir que un asunto contenga solo letras mayúsculas.',
+'Require e-mail label'		=>	'Requerir el correo electrónico de los visitantes',
+'Require e-mail help'		=>	'Requerir que los visitantes proporcionen una dirección de correo electrónico cuando publiquen mensajes.',
+'Signatures subhead'		=>	'Firmas',
+'BBCode sigs label'			=>	'BBCodes en las firmas',
+'BBCode sigs help'			=>	'Permitir el uso de BBCodes en las firmas de usuarios.',
+'Image tag sigs label'		=>	'Etiqueta img en las firmas',
+'Image tag sigs help'		=>	'Permitir el uso de la etiqueta BBCode [img][/img] en los mensajes. (no recomendado).',
+'All caps sigs label'		=>	'Firmas usando solo mayúsculas',
+'All caps sigs help'		=>	'Permitir que una firma contenga solo letras mayúsculas.',
+'Max sig length label'		=>	'Longitud máxima de las firmas',
+'Max sig length help'		=>	'El número máximo de caracteres que una firma de usuario puede contener.',
+'Max sig lines label'		=>	'Número máximo de líneas en la firma',
+'Max sig lines help'		=>	'El número máximo de líneas que una firma de usuario puede contener.',
+'Registration subhead'		=>	'Registro',
+'Banned e-mail label'		=>	'Permitir direcciones de correo electrónico suspendidas',
+'Banned e-mail help'		=>	'Permitir a los usuarios registrarse con, o cambiar a, una dirección/dominio de correo electrónico suspendida. Si se conserva el valor por defecto (sí), esta acción será permitida, pero un correo electrónico de alerta será enviado a la lista de correo (una manera efectiva de detectar múltiples registros).',
+'Duplicate e-mail label'	=>	'Permitir direcciones de correo electrónico duplicadas',
+'Duplicate e-mail help'		=>	'Controlar si es que a los usuarios de les permite registrarse con un correo electrónico que pertenezca a otro usuario. Si está permitido, un correo electrónico de alerta será enviado a la lista de correo, en caso de que se detecte un duplicado.',
+
+);
